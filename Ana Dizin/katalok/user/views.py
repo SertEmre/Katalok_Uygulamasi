aa2e3a5f6204e309ca4e92d39fb01e66cc3ee97e
SertEmre <emresert2233@gmail.com>
2025-06-12 20:25:23 +0300

    Register ve Login işlemlerinin kontrollerini ekledim

--- Ana Dizin/katalok/user/views.py
+++ Ana Dizin/katalok/user/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -6,7 +7,35 @@ def login(request):
     return render(request, 'user/login.html')
 
 def register(request):
-    return render(request, 'user/register.html')
+    if request.method == 'POST':
+        
+        #get from values
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repassword = request.POST['repassword']
+
+        if password == repassword:
+            #Username
+            if User.objects.filter(username= username).exists():
+                print("Bu kullanıcı adı daha önce alınmış")
+                return redirect('register')
+            else:
+                if User.objects.filter(email= email).exists():
+                    print("Bu email daha önce alınmış")
+                    return redirect('register')
+                else:
+                    #her şey doğruysa 
+                    user = User.objects.create_user(username=username, password=password, email=email)
+                    user.save()
+                    print('kullanıcı oluşturuldu')
+                    return redirect('login')
+        else:
+            print("Parolalar eşleşmiyor")
+            return redirect('register')
+        
+    else:
+        return render(request, 'user/register.html')
 
 def logout(request):
     return render(request, 'user/logout.html')
